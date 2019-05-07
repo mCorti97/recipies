db37e209296146759c776c9bddd6dd40996506c0
Mattia Corti <Matt@iMac-di-Mattia.homenet.telecomitalia.it>
2019-05-07 19:02:07 +0200

    Esercizio vacanze

--- js/controllers/recipie.js
+++ js/controllers/recipie.js
@@ -11,10 +11,15 @@ function loadRecipie() {
     var recipieId = getIdFromURL();
     db.collection('recipies')
     .doc(recipieId)
-    .onSnapshot(function(doc){
-        recipie = doc.data();
-        console.log(recipie);
+    .onSnapshot(function(item){
+       var recipie = item.data();
+       var recipieId = findGetParameter('id');
+       console.log(recipieId);
+
+       addRecipie(recipie, recipieId);
+
     })
+
 }
 
 function getIdFromURL() {
@@ -31,7 +36,38 @@ function findGetParameter(parameterName) {
             tmp = item.split("=");
             if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
         });
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const myParam = urlParams.get('myParam');
+    
+        
     return result;
 }
 
+function addRecipie(recipie, recipieId){
+
+    var title = recipie.name;
+    var difficulty = recipie.difficulty;
+    var description = recipie.description;
+    var cookingTime = recipie.cookingTime;
+    var preparationTime = recipie.preparationTime;
+    var people = recipie.people;
+    var cost = recipie.cost;
+
+    console.log(title);
+    $('#titolo').text(title);
+    $('#difficulty').text('Difficoltà: ' + difficulty);
+    $('#description').text(description);
+    $('#cookingTime').text('Tempo di cottura: ' + cookingTime + ' minuti');
+    $('#preparationTime').text('Tempo di preparazione: ' + preparationTime + ' minuti');
+    $('#people').text('Dosi per: '+ people + ' persone');
+    $('#cost').text('Costo: ' + cost);
+
+}
+
+
+
 loadRecipie();
+
+
+
